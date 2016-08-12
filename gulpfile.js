e13ceb769b1a929b2da9d68b67a1772dcb72beff
memoma <gminiala@gmail.com>
2016-08-12 23:52:55 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,133 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var htmlreplace = require('gulp-html-replace');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var reactify = require('reactify');
-var streamify = require('gulp-streamify');
-var connect = require('gulp-connect');
-var stylus = require('gulp-stylus');
+var gulp         = require('gulp');
+var uglify       = require('gulp-uglify');
+var source       = require('vinyl-source-stream');
+var browserify   = require('browserify');
+var watchify     = require('watchify');
+var reactify     = require('reactify');
+var streamify    = require('gulp-streamify');
+var connect      = require('gulp-connect');
+var rename       = require('gulp-rename');
+var clean        = require('gulp-clean');
+var base64       = require('gulp-base64');
 
-var path = {
-  SRC_HTML: 'src/html/index.html',
-  DEST_HTML: 'dist/index.html',
-  MINIFIED_OUT: 'build.min.js',
-  OUT: 'build.js',
-  DEST: 'dist',
-  DEST_BUILD: 'dist/build/js',
-  DEST_SRC: 'dist/src/js',
-  ENTRY_POINT: './src/js/app.js',
-  SRC_CSS: './src/css/',
-  DEST_BUILD_CSS: 'dist/build/css',
-  DEST_SRC_CSS: 'dist/src/css',
-  OUT_CSS: 'style.css',
-  MINIFIED_OUT_CSS: 'style.min.css'
-};
+// 路径
+var paths = {
+	html       : './public/html',
+	local      : './public/local',
+	staging    : './public/staging',
+	production : './public/production',
+}
+
+// 入口
+var entries = 'app';
+
+// 公用模块，打包至bundle-require
+var external = ['react', 'react-dom', 'react-router'];
 
 // Liveload
-gulp.task('connect', function() {
-  connect.server({
-    root: './',
-    livereload: true
-  });
-});
+gulp.task('connect', function () {
+	connect.server({
+		root: './public',
+		livereload: true
+	})
+})
 
-// reload when js change, use build.js
-gulp.task('js', function() {
-  gulp.src(path.DEST_SRC + '/*.js')
-    .pipe(connect.reload());
-});
-// reload when css change, use style.css
-gulp.task('css', function() {
-  gulp.src(path.DEST_SRC_CSS + '/*.css')
-    .pipe(connect.reload());
-});
+// 监视，游览器实时更新，打包到 staging 目录
+gulp.task('watch', function () {
+	// Css
+	gulp.watch([paths.local + '/**/*.css'], function () {
+		gulp.src(paths.local + '/**/*.css')
+			.on('error', function(err){console.log(err.message);})
+			.on('end', function(){ console.log('Updated Css: ' + new Date()); })
+			.pipe(autoprefixer('last 2 versions'))
+			.pipe(gulp.dest(paths.staging))
+			.pipe(connect.reload())
+	})
+	// Js
+	var b = browserify({
+		entries      : [paths.local + '/js/' + entries + '.js'],
+		transform    : [reactify],
+		debug        : true,
+		cache        : {},
+		packageCache : {},
+		fullPaths    : true,
+		plugin       : [watchify]
+	})
 
-gulp.task('watch', function() {
-  gulp.watch([path.DEST_SRC + '/*.js'], ['js']); // watch for reload
-  gulp.watch([path.DEST_SRC_CSS + '/*.css'], ['css']); // watch for reload
+	var bundle = function () {
+		b.external(external);
+		b.bundle()
+			.on('error', function(err){console.log(err.message);})
+			.on('end', function(){ console.log('Updated Js: ' + entries + ' ' + new Date()); })
+			.pipe(source('bundle-' + entries + '.js'))
+			.pipe(gulp.dest(paths.staging + '/js/bundle-' + entries))
+			.pipe(connect.reload())
+	}
 
-  // watch && transform styl to css
-  gulp.watch(['./src/css/style.styl'], function() {
-    gulp.src('./src/css/style.styl')
-      .pipe(stylus({ set:['compress', path.OUT_CSS] }))
-      .pipe(gulp.dest(path.DEST_SRC_CSS))
-  });
+	b.on('update', function () {
+		bundle()
+	})
 
-  // watch && transform jsx to js
-  var watcher  = watchify(browserify({
-    entries: [path.ENTRY_POINT],
-    transform: [reactify],
-    debug: true,
-    cache: {}, packageCache: {}, fullPaths: true
-  }));
+	bundle();
+})
 
-  watcher.on('update', function () {
-    watcher.bundle()
-      .pipe(source(path.OUT))
-      .pipe(gulp.dest(path.DEST_SRC))
-      console.log('Updated Js');
-  })
-    .bundle()
-    .pipe(source(path.OUT))
-    .pipe(gulp.dest(path.DEST_SRC));
-});
+// 开发环境
+gulp.task('staging', function () {
+	var b = browserify({
+		entries      : [paths.local + '/js/' + entries + '.js'],
+		transform    : [reactify],
+		debug        : true,
+		cache        : {},
+		packageCache : {},
+		fullPaths    : true,
+	})
+	// 来自公用包模块
+	b.external(external);
+	b.bundle()
+		.on('error', function(err){console.log(err.message);})
+		.on('end', function(){ console.log('Updated Js: ' + entries + ' ' + new Date()); })
+		.pipe(source('bundle-' + entries + '.js'))
+		.pipe(gulp.dest(paths.staging + '/js/bundle-' + entries))
+})
 
-gulp.task('build', function(){
-  browserify({
-    entries: [path.ENTRY_POINT],
-    transform: [reactify],
-  })
-    .bundle()
-    .pipe(source(path.MINIFIED_OUT))
-    .pipe(streamify(uglify()))
-    .pipe(gulp.dest(path.DEST_BUILD));
+// 公用 require 包
+gulp.task('require-staging', function () {
+	var b = browserify({
+		require : external
+	})
+	b.bundle()
+		.on('error', function(err){console.log(err.message);})
+		.on('end', function(){ console.log('Updated Js: require ' + new Date()); })
+		.pipe(source('bundle-require.js'))
+		.pipe(gulp.dest(paths.staging + '/js/bundle-require'))
+})
 
-  // css
-  gulp.src('./src/css/style.styl')
-      .pipe(stylus({compress: true, set:['compress', path.MINIFIED_OUT_CSS] }))
-      .pipe(gulp.dest(path.DEST_BUILD_CSS))
-});
+// 生产环境：app 包
+gulp.task('production', function () {
+	// 清除该文件夹文件
+	gulp.src(paths.production + '/js/bundle-' + entries + '/*.js', {read: false})
+		.pipe(clean());
+	// 重命名，压缩
+	gulp.src(
+		paths.staging + '/js/bundle-' + entries + '/bundle-' + entries + '.js'
+	)
+	  .pipe(rename('bundle-' + entries + '-min.js'))
+	  .pipe(streamify(uglify()))
+	  .pipe(gulp.dest(paths.production + '/js/bundle-' + entries));
+})
 
-gulp.task('replaceBuildJs', function(){
-  gulp.src(path.SRC_HTML)
-    .pipe(htmlreplace({
-      'js': 'build/js/' + path.MINIFIED_OUT
-    }))
-    .pipe(gulp.dest(path.DEST));
-});
+// 生产环境：公用 require 包 
+gulp.task('require-production', function () {
+	// 清除该文件夹文件
+	gulp.src(paths.production + '/js/bundle-require/*.js', {read: false})
+		.pipe(clean());
+	// 重命名，压缩
+	gulp.src(
+		paths.staging + '/js/bundle-require/bundle-require.js'
+	)
+	  .pipe(rename('bundle-require-min.js'))
+	  .pipe(streamify(uglify()))
+	  .pipe(gulp.dest(paths.production + '/js/bundle-require'));
+})
 
-gulp.task('replaceSrcJs', function(){
-  gulp.src(path.SRC_HTML)
-    .pipe(htmlreplace({
-      'js': 'src/js/' + path.OUT
-    }))
-    .pipe(gulp.dest(path.DEST));
-});
-
-gulp.task('production', ['replaceBuildJs', 'build']);
-
-gulp.task('default', ['connect', 'watch', 'replaceSrcJs']);
+gulp.task('default', ['connect', 'watch'])
